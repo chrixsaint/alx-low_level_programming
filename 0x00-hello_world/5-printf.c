7825e2d9ddb337ae5813abe4e6fa3266869d4605
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-08-10 11:59:38 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A  C program that prints exactly with print f
+ * Return: 0 (succes)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
