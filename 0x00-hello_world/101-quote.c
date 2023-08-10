85c7f991009cd4b7656a9c89c5b9a24d70b2d3c7
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-08-10 14:33:13 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints exactly
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19", 59);
+	return (1);
+}
