d00eb0f0c370d9b8ea2048df631e0e68601088e5
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-08-16 09:10:18 -0700

    task 3

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - causes of an infinit loop
+ *Return: (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("inifint loop incoming :(\n");
+
+	i = 0;
+
+	/**
+	 *while (i < 10)
+	 *{
+	 *putchar(i);
+	 }
+	 */
+	printf("infinit loop avoided! \\o/\n");
+	return (0);
+}
