d2671989d98a9c29062de1cbbd30262429766d61
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-08-17 03:48:31 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	printf("inifint loop incoming :(\n");
+	printf("inifinte loop incoming :(\n");
 
 	i = 0;
 
@@ -19,7 +19,6 @@ int main(void)
 	 *	putchar(i);
 	 *}
 	 */
-
-	printf("infinit loop avoided! \\o/\n");
+	printf("infinite loop avoided! \\o/\n");
 	return (0);
 }
