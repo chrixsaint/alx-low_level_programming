11b2fdf3e0ac42bacbfe64b9585ec54d1becc50b
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-08-17 14:29:30 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - print 0 to 10
+ *Return: void
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = 'O'; c <= '9'; c++)
+	{
+		if (!(c == '2' || c == '4'))
+		{
+			_putchar('c');
+		}
+	_putchar('\n');
+	}
+}
