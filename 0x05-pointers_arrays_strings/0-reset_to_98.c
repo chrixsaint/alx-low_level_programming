88505004bdc696ed0ede0476fbd004b992db224d
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-08-20 23:48:11 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes an int as parameter and updates it to 98
+ *@n: - the value to be updated to 98
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
