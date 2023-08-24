e08005a81979da5087c5b3bbc1bdab1163a7370d
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-08-24 11:21:38 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,23 +1,25 @@
 #include "main.h"
+
 /**
  * leet - a function that encodes a string into 1337
  * @str: set of strings to be encoded
  *
  * Return: Encoded string
  */
+
 char *leet(char *str)
 {
-	int i, j;
-	char c[] = "aAeEoOtTlL";
-	char d[] = "4433007711";
+	char *leet_chars = "AaEeOoTtLl";
+	char *leet_nums = "44330771";
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (int i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; c[j] != '\0'; j++)
+		for (int j = 0; leet_chars[j] != '\0'; j++)
 		{
-			if (str[i] == c[j])
+			if (str[i] == leet_chars[j])
 			{
-				str[i] = d[j];
+				str[i] = leet_nums[j];
+				break;
 			}
 		}
 	}
