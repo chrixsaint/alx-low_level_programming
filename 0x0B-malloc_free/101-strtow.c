d4c6db4ab119c536fb994afb29bd5f4bbd70faca
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-09-05 08:10:32 -0700

    task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,106 +1,45 @@
 #include <stdio.h>
-#include <string.h>
-#include "main.h"
-#include <stdlib.h>
-
 /**
- * is_space - main entry
- * @c: input
- * Return: c when successful
+ * main - print numbers in 3s separaed by , and space
+ *
+ * Return: (0)
+ *
  */
-
-int is_space(char c)
+int main(void)
 {
-	return (c == ' ' || c == '\t' || c == '\n');
-}
+	int num1, num2, num3;
 
-/**
- * count_words - main entry
- *@str: string
- *Return: number of words
- */
+	num1 = '0';
+	num2 = '0';
+	num3 = '0';
 
-int count_words(char *str)
-{
-	int num_words, in_word;
-
-	while (*str && is_space(*str))
+	while (num1 <= '9')
 	{
-		str++;
-	}
-	while (*str)
-	{
-		if (!is_space(*str))
+		while (num2 <= '9')
 		{
-			if (!in_word)
+			num3 = '0';
+			while (num3 <= '9')
 			{
-				num_words++;
-				in_word = 1;
+				if (num1 < num2 && num2 < num3)
+				{
+					putchar(num1);
+					putchar(num2);
+					putchar(num3);
+
+					if (num1 != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				num3++;
 			}
+			num2++;
 		}
-		else
-		{
-			in_word = 0;
-		}
-		str++;
+		num1++;
+		num2 = '0';
 	}
-	return (num_words);
-}
+	putchar('\n');
 
-/**
- * split_words - function that splits word
- * @str: string
- * @num_words: number of words
- *Return: words when successful
- */
-
-char **split_words(char *str, int num_words)
-{
-	char **words, *p, *start;
-	int word_len, i, j;
-
-	words = (char **)malloc((num_words + 1) * sizeof(char *));
-	if (words == NULL)
-		return (NULL);
-	i = 0;
-	for (p = str; *p; p++)
-	{
-		if (!is_space(*p))
-		{
-			start = p;
-			while (*p && !is_space(*p))
-				p++;
-			word_len = p - start;
-			words[i] = (char *)malloc((word_len + 1) * sizeof(char));
-			if (words[i] == NULL)
-			{
-				for (j = 0; j < i; j++)
-					free(words[j]);
-				free(words);
-				return (NULL);
-			}
-			strncpy(words[i], start, word_len);
-			words[i][word_len] = '\0';
-			i++;
-		}
-	}
-	words[num_words] = NULL;
-	return (words);
-}
-/**
- * strtow - main entry
- * @str: string
- * Return: split_words, str and num_words when successful
- */
-
-char **strtow(char *str)
-{
-	int num_words;
-
-	if (str == NULL || *str == '\0')
-		return (NULL);
-	num_words = count_words(str);
-	if (num_words == 0)
-		return (NULL);
-	return (split_words(str, num_words));
+	return (0);
 }
