cdd7376c3bbc4d1d53399c414313dd8f7826530c
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-09-05 07:18:47 -0700

    task 101

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * is_space - main entry
+ * @c: input
+ * Return: c when successful
+ */
+
+int is_space(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n');
+}
+
+/**
+ * count_words - main entry
+ *@str: string
+ *Return: number of words
+ */
+
+
+int count_words(char *str)
+{
+	char *p;
+	int num_words;
+
+	num_words = 0;
+	for (p = str; *p; p++)
+		if (!is_space(*p) && (p == str || is_space(*(p - 1))))
+			num_words++;
+	return (num_words);
+}
+
+/**
+ * *split_words - function that splits word
+ * @str: string
+ * @num_words: number of words
+ *Return: words when successful
+ */
+
+char **split_words(char *str, int num_words)
+{
+	char **words, *p, *start;
+	int word_len, i, j;
+
+	words = (char **)malloc((num_words + 1) * sizeof(char *));
+	if (words == NULL)
+		return (NULL);
+	i = 0;
+	for (p = str; *p; p++)
+	{
+		if (!is_space(*p))
+		{
+			start = p;
+			while (*p && !is_space(*p))
+				p++;
+			word_len = p - start;
+			words[i] = (char *)malloc((word_len + 1) * sizeof(char));
+			if (words[i] == NULL)
+			{
+				for (j = 0; j < i; j++)
+					free(words[j]);
+				free(words);
+				return (NULL);
+			}
+			strncpy(words[i], start, word_len);
+			words[i][word_len] = '\0';
+			i++;
+		}
+	}
+	words[num_words] = NULL;
+	return (words);
+}
+/**
+ * **strtow - main entry
+ * @str: string
+ * Return: split_words, str and num_words when successful
+ */
+
+char **strtow(char *str)
+{
+	int num_words;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	num_words = count_words(str);
+	if (num_words == 0)
+		return (NULL);
+	return (split_words(str, num_words));
+}
