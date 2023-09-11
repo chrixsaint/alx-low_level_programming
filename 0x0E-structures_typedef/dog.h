2e2492ab67be9bff3abb901a871ebf8c22686401
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-09-11 12:30:00 +0100

    struct dog - Definess a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,20 +12,13 @@
  * and the owner's name (a character string).
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
-/**
- * dog_t - Typedef for struct dog
- *
- * Description: A new type dog_t as a new name for the type struct dog.
- */
-
-typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
