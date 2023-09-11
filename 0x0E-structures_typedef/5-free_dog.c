4b184e92ecda034b1348dd68c1beaf41707b5b6a
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-09-11 14:12:52 +0100

    free_dog - Frees memory allocated for a dog_t struct

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,12 +2,22 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * free_dog - Frees memory allocated for a dog_t struct
+ * @d: Pointer to the dog_t struct to be freed
+ *
+ * This function takes a pointer to a dog_t struct, checks if the 
+ * pointer is not NULL, and if it's not, frees the memory allocated 
+ * for the 'name' and 'owner' fields,
+ * as well as the struct itself. This function does not return any value.
+ */
+ 
 void free_dog(dog_t *d)
 {
-    if (d)
-    {
-        free(d->name);
-        free(d->owner);
-        free(d);
-    }
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
---
+++ 0x0E-structures_typedef/rough.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - Creates a new dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: This function creates a new dog with the provided name,
+ * age, and owner. It stores a copy of name and owner. If the function
+ * fails, it returns NULL.
+ *
+ * Return: A pointer to the new dog, or NULL if the function fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog;
+
+	newDog = malloc(sizeof(dog_t));
+
+	if (newDog == NULL)
+	{
+		return (NULL);
+	}
+	newDog->name = strdup(name);
+	if (newDog->name == NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
+
+	newDog->age = age;
+
+	newDog->owner = strdup(owner);
+	if (newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog);
+		return (NULL);
+	}
+
+	return (newDog);
+}
+
