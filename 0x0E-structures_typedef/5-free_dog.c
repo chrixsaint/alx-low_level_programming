f92d769bd448228fad481e70b9b14f7f1e756e1c
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-09-11 14:21:52 +0100

    free_dog - Frees memory allocated for a dog_t struct

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -6,12 +6,12 @@
  * free_dog - Frees memory allocated for a dog_t struct
  * @d: Pointer to the dog_t struct to be freed
  *
- * This function takes a pointer to a dog_t struct, checks if the 
- * pointer is not NULL, and if it's not, frees the memory allocated 
- * for the 'name' and 'owner' fields,
- * as well as the struct itself. This function does not return any value.
+ * This function takes a pointer to a dog_t struct, checks if
+ * the pointer is not NULL, and if it's not, frees the memory
+ * allocated for the 'name' and 'owner' fields, as well as the struct
+ * itself. This function does not return any value.
  */
- 
+
 void free_dog(dog_t *d)
 {
 	if (d)
