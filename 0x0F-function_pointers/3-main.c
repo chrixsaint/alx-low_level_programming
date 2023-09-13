0e7d798ee85946bc22677cbae8082ea8875aecaf
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-09-13 17:45:28 +0100

    a program that performs simple operations.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
-	int (*operation)(int, int), result;
+	int (*operation)(int, int);
 	char *operator;
 
 	if (argc != 4)
@@ -14,16 +14,16 @@ int main(int argc, char *argv[])
 		return (98);
 	}
 	num1 = atoi(argv[1]);
-	*operator = argv[2];
+	operator = argv[2];
 	num2 = atoi(argv[3]);
 
-	(*operation)(int, int) = get_op_func(operator);
+	operation = get_op_func(operator);
 	if (operation == NULL)
 	{
 		printf("Error\n");
 		return (99);
 	}
-	result = operation(num1, num2);
+	result = (*operation)(num1, num2);
 	printf("%d\n", result);
 
 	return (0);
