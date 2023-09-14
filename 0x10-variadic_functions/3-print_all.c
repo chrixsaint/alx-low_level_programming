cc8ae2ba8ef39c9fbb72714cc1c8138ee0257f57
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-09-14 13:14:32 +0100

    a function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,49 +1,96 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "variadic_functions.h"
 
+/**
+ * print_char - function prints char
+ * @arg: argument
+ */
+
+void print_char(va_list arg)
+{
+	char c = va_arg(arg, int);
+
+	printf("%c", c);
+}
 
 /**
- * print_all - prints anything
- * @format: list of types of arguments passed to the function
+ * print_int - function prints int
+ * @arg: argument
  */
+
+void print_int(va_list arg)
+{
+	int i = va_arg(arg, int);
+
+	printf("%d", i);
+}
+
+/**
+ * print_float - function prints float
+ * @arg: argument
+ */
+
+void print_float(va_list arg)
+{
+	float f = va_arg(arg, double);
+
+	printf("%f", f);
+}
+
+/**
+ * print_string - function prints strings.
+ * @arg: argument
+ */
+
+void print_string(va_list arg)
+{
+	char *s = va_arg(arg, char *);
+
+	if (s == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", s);
+}
+
+/**
+ * print_all - function that prints anything.
+ * @format: format specifier
+ */
+
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	char *str;
+	va_list arg;
+	int i, j;
 	char *sep = "";
-	int i = 0;
 
-	va_start(args, format);
+	print_d data[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
 
-	while (format[i])
+	va_start(arg, format);
+
+	i = 0;
+	while (format && *(format + i))
 	{
-		switch (format[i++])
+		j = 0;
+		while (j < 4 && *(format + i) != *(data[j].c))
 		{
-			case 'c':
-				printf("%s%c", sep, va_arg(args, int));
-				sep = ", ";
-				break;
-			case 'i':
-				printf("%s%d", sep, va_arg(args, int));
-				sep = ", ";
-				break;
-			case 'f':
-				printf("%s%f", sep, va_arg(args, double));
-				sep = ", ";
-				break;
-			case 's':
-				str = va_arg(args, char*);
-				if (str == NULL)
-				{
-					str = "(nil)";
-				}
-				printf("%s%s", sep, str);
-				sep = ", ";
-				break;
+			j++;
 		}
+
+		if (j < 4)
+		{
+			printf("%s", sep);
+			data[j].f_pr(arg);
+			sep = ", ";
+		}
+		i++;
 	}
 	printf("\n");
 
-	va_end(args);
+	va_end(arg);
 }
