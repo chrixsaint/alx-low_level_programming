9574f006ffb0a77329fc620f40c8a3ceac59b388
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-09-14 13:25:52 +0100

    a function that prints anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,22 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct print_d - Struct print_d
+ *
+ * @c: The operator
+ * @f_pr: The function associated
+ */
+typedef struct print_d
+{
+	char *c;
+	void (*f_pr)(va_list a);
+} print_d;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
