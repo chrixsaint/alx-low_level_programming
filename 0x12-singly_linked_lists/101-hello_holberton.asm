4c5d72f23b7566af49e8dcf2ed20dcddaf2f3b56
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-09-21 23:32:39 +0100

    a 64-bit program in assembly that prints Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,16 @@
-section .data
-    hello db 'Hello, Holberton', 10, 0 ; 10 is the ASCII value for newline
+	SECTION .data
+chrix:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-section .text
-    extern printf
-    global main
-
+	SECTION .text
+	extern printf
+	global main
 main:
-    ; prepare arguments for printf
-    mov rdi, hello
-    xor rax, rax ; zero out rax to indicate no floating point values are passed
+	mov esi, chrix
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-    ; call printf
-    call printf
-
-    ; return from main
-    mov eax, 60 ; system call number for exit
-    xor edi, edi ; exit code 0
-    syscall ; make the system call
+	mov eax, 0
+	ret
+	
