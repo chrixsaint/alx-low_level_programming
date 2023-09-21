bc784ffc7ddae554e12adbae297aad0e14da4f65
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-09-21 18:37:03 +0100

    header file lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,7 +8,7 @@
 
 
 /**
- * struct list_s - singly linked list
+ * struct list_s - singly linked list.
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
