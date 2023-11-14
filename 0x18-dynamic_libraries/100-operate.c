a398b00713cb7429d4fca7d1c0014f618a25e0f1
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-11-14 10:54:45 +0100

    task 100

---
+++ 0x18-dynamic_libraries/100-operate.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+/**
+ * add - this does what you see.
+ * @a: Operand 1.
+ * @b: operand 2.
+ * Return: returns result.
+ */
+int add(int a, int b)
+{
+	return a + b;
+}
+/**
+ * sub - this does what you see.
+ * @a: Operand 1.
+ * @b: operand 2.
+ * Return: returns result.
+ */
+int sub(int a, int b)
+{
+	return a - b;
+}
+/**
+ * mul - this does what you see.
+ * @a: Operand 1.
+ * @b: operand 2.
+ * Return: returns result.
+ */
+int mul(int a, int b)
+{
+	return a * b;
+}
+/**
+ *div - this does what you see.
+ * @a: Operand 1.
+ * @b: operand 2.
+ * Return: returns result.
+ */
+int div(int a, int b)
+{
+	return a / b;
+}
+/**
+ * mod - this does what you see.
+ * @a: Operand 1.
+ * @b: operand 2.
+ * Return: returns result.
+ */
+int mod(int a, int b)
+{
+	return 	a % b;
+}
